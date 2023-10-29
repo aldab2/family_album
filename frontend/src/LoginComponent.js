796f8ec5b548d5579a231daaac3bb4dcc325c4fa
Abdullah Barnawi <aabarnawi@DESKTOP-N28GE6D.localdomain>
2023-10-29 22:42:18 +0300

    add login component

---
+++ frontend/src/LoginComponent.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function LoginComponent() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here, e.g., API call, etc.
+    console.log('Email:', email, 'Password:', password);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginComponent;
