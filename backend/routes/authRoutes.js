d722802533177d2a3a82a8e7cab5fcf9323ffb22
Abdullah Abdullatif M Barnawi <aabarnawi@TAHAKOM.COM>
2023-11-06 17:25:05 +0300

    feat: delete user

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,26 +1,26 @@
 import express from 'express';
-import {authUser,
+import {login,
     registerFamily,
     getFamilyProfile,
     editFamilyProfile,
     deleteFamilyProfile,
     getUserProfile,
-    editUser,
-    deleteUser,
     logoutUser,
     addFamilyMember,
-    changePassword
+    changePassword,
+    deleteFamilyMember,
+    editFamilyMember
 }  from '../controllers/authController.js';
 import { protect } from '../middleware/authMidleware.js';
 
 const router = express.Router();
 
-router.post('/',authUser);
+router.post('/',login);
 router.post('/family',registerFamily);
 router.get('/family',protect,getFamilyProfile);
 router.put('/family',protect,editFamilyProfile);
 router.delete('/family',protect,deleteFamilyProfile);
-router.route('/user').post(protect,addFamilyMember).get(protect,getUserProfile).put(protect,editUser).delete(protect,deleteUser)
+router.route('/user').post(protect,addFamilyMember).get(protect,getUserProfile).put(protect,editFamilyMember).delete(protect,deleteFamilyMember)
 router.put('/change-password',protect,changePassword)
 router.post('/logout',logoutUser);
 
