2c046908f7f8f0dc77a5216a4cec1fc6882d1cef
Abdullah Barnawi <aabarnawi@DESKTOP-N28GE6D>
2024-01-31 22:23:12 +0300

    backend rate-limiting on email and fixes for fe email verification

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -14,12 +14,22 @@ import {login,
 }  from '../controllers/authController.js';
 import { sendVerificationEmail } from '../utils/emailUtils.js';
 import { protect } from '../middleware/authMidleware.js';
-
+import rateLimit from 'express-rate-limit';
 const router = express.Router();
+
+
+// Create rate limit middleware configured for 1 request per minute
+const sendVerificationEmailLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 1 minute in milliseconds
+  max: 1, // Limit each IP to 1 request per `window` (here, per minute)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: {message: "You can only send one verification email per minute"}, // Custom message
+}); 
 
 router.post('/',login);
 router.post('/family',registerFamily);
-router.get('/send-verification-email',protect, sendVerificationEmail);
+router.get('/send-verification-email',protect,sendVerificationEmailLimiter, sendVerificationEmail);
 router.post('/verify-code',protect, verifyCode);
 router.get('/family',protect,getFamilyProfile);
 router.put('/family',protect,editFamilyProfile);
